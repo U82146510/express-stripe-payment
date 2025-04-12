9a3e875b70c3351f13eaedca72ce26f48ed15620
U82146510 <U82146510@protonmail.com>
2025-04-12 13:45:46 +0100

    updating webhook

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -1,6 +1,9 @@
 import { type Request,type Response,type NextFunction } from "express";
 import Stripe from "stripe";
 import dotenv from 'dotenv';
+
+
+
 dotenv.config();
 
 const stripe_api = process.env.stripe;
@@ -19,10 +22,10 @@ const stripe = new Stripe(stripe_api)
 
 export const webhook = (req:Request,res:Response,next:NextFunction)=>{
     const sig = req.headers['stripe-signature'];
-    let event;
+    let event:Stripe.Event;
     try {
         event = stripe.webhooks.constructEvent(req.body,sig!,hook);
-    } catch (error) {
+    } catch (error:any) {
         console.error('webhook signature verification failed');
         res.status(400).json({message:'webhook error'});
         return;
@@ -30,7 +33,13 @@ export const webhook = (req:Request,res:Response,next:NextFunction)=>{
     
 
     try {
-
+        if(event.type==='checkout.session.completed'){
+            const session = event.data.object as Stripe.Checkout.Session;
+            
+            console.log("customer email",session.customer_email);
+            console.log('Amount:',session.amount_total);
+            res.status(200).send('Received')
+        }
     } catch (error) {
         next(error)
     }
--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -1,5 +1,6 @@
 import {payment} from '../controllers/payment';
 import { Router } from 'express';
+import bodyParser from 'body-parser';
 
 export const user:Router = Router();
-user.post('/checkout',payment);
+user.post('/checkout',bodyParser.raw({ type: 'application/json' }),payment);
